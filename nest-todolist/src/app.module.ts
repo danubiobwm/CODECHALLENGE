367c784ef5d7608e2f62879476090e7e246c06bc
danubiobwm <danubio.bwm@gmail.com>
2021-02-08 12:28:43 -0300

    Back TodoList finish

--- nest-todolist/src/app.module.ts
+++ nest-todolist/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'todo',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nest-todolist/src/todo/todo.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Todo {
+  constructor(desc: string) {
+    this.description = desc;
+  }
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  description: string;
+  @Column({ default: false })
+  done: boolean;
+}
