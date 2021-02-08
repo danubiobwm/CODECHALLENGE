1934c8d8aaca083bea951426fdab23356304b212
danubiobwm <danubio.bwm@gmail.com>
2021-02-08 14:46:29 -0300

    frontend angular finalizado

--- angular-todo/src/app/app-routing.module.ts
+++ angular-todo/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 import { CommonModule } from '@angular/common';
 
 
+const routes: Routes=[
+  {path: '', component: TodoComponent}
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
